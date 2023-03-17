5ec499f59b02d7da548c18bbc3c3439907190fea
MarinaUG <marina.unamuno@gmail.com>
2023-03-17 13:52:58 +0100

    exercici5

--- src/films.js
+++ src/films.js
@@ -8,41 +8,56 @@ function getAllDirectors(movies) {
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(movies, director) {
 
-  const result =movies.filter(item => item.director===director);
-    console.log("EXERCICE 2 ->", result);
-    return result;
-  
+  const result = movies.filter(item => item.director === director);
+  console.log("EXERCICE 2 ->", result);
+  return result;
+
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 
-function average(array){
-  const averageNumber=Number((array.reduce( (acc, item) => Number(acc)+Number(item.score), 0) /array.length).toFixed(2));
-  console.log ("mitja",averageNumber);
+function average(array) {
+  const averageNumber = Number((array.reduce((acc, item) => Number(acc) + Number(item.score), 0) / array.length).toFixed(2));
+  console.log("mitja", averageNumber);
   return averageNumber;
 }
 
 function moviesAverageOfDirector(movies, director) {
-  const movieDirector=getMoviesFromDirector(movies, director);
-  const result=average(movieDirector);
+  const movieDirector = getMoviesFromDirector(movies, director);
+  const result = average(movieDirector);
   console.log("Exercici 3", result);
   return result;
- }
+}
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(movies) {
-const movie=movies.map(item=>item.title);
-movie.sort(); 
-if (movie.length>20){
-  movie.length=20;
-}
-console.log("Exercici 4", movie);
-return movie;
+  const movie = movies.map(item => item.title);
+  movie.sort();
+  if (movie.length > 20) {
+    movie.length = 20;
+  }
+  console.log("Exercici 4", movie);
+  return movie;
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
+function orderByYear(movies) {
+  const sortedMovie = [...movies].sort((item1, item2) => {
+   if (item1.year > item2.year) {
+      return 1;
+    }
+    if (item1.year < item2.year) {
+      return -1;
+    }
+    if (item1.title.toLowerCase() > item2.title.toLowerCase()) {
+      return 1;
+    }
+    if (item1.title.toLowerCase() < item2.title.toLowerCase()) {
+      return -1;
+    }
+  });
+  console.log("Exercici 5", sortedMovie);
+  return sortedMovie;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
@@ -57,7 +72,7 @@ function hoursToMinutes() {
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
-  
+
 }
 
 
